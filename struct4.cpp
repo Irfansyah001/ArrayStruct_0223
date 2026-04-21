bccf633fd59cda30e32baf32f1b0fdd0312420d0
汉堡包 <syahirfan545@gmail.com>
2026-04-21 10:13:09 +0700

    copy from struct3.cpp to struct4.cpp

---
+++ struct4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa {
+    string NIM;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa: ";
+    getline(cin, mhs.NIM);
+    cout << "Nama Mahasiswa: ";
+    getline(cin, mhs.nama);
+    cout << "Alamat Mahasiswa: " << endl;
+    cout << "\t Nama Desa: ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota: ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur Mahasiswa: ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM: " << mhs.NIM;
+    cout << "\n Nama: " << mhs.nama;
+    cout << "\n Alamat: ";
+    cout << "\n Desa: " << mhs.alamat.desa;
+    cout << "\n Kota: " << mhs.alamat.kota;
+    cout << "\n Umur: " << mhs.umur;
+
+    return 0;
+}
