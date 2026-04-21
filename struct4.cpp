96b186e6eeb2c19387a2393594152dc105e7b089
汉堡包 <syahirfan545@gmail.com>
2026-04-21 10:40:32 +0700

    Update input method for NIM and nama fields to handle leading whitespace

--- struct4.cpp
+++ struct4.cpp
@@ -19,9 +19,9 @@ int main() {
     for (int i = 0; i < 3; i++) {
         cout << "Data Mahasiswa ke-" << (i + 1) << ": " << endl;
         cout << "Nomor Mahasiswa: ";
-        getline(cin, mhs[i].NIM);
+        getline(cin >> ws, mhs[i].NIM);
         cout << "Nama Mahasiswa: ";
-        getline(cin, mhs[i].nama);
+        getline(cin >> ws, mhs[i].nama);
         cout << "Alamat Mahasiswa: " << endl;
         cout << "\t Nama Desa: ";
         cin >> mhs[i].alamat.desa;
