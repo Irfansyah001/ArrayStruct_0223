d1b987a253b2d44cc3217b12d86cf33a21bce9a2
汉堡包 <syahirfan545@gmail.com>
2026-04-21 09:43:45 +0700

    Implement main function to input and display Mahasiswa details

--- struct2.cpp
+++ struct2.cpp
@@ -13,3 +13,20 @@ struct Mahasiswa {
     int umur;
 };
 
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa: ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa: ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa: ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa: ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM: " << mhs.NIM;
+    cout << "\n Nama: " << mhs.nama;
+    cout << "\n Alamat: " << mhs.alamat;
+    cout << "\n Umur: " << mhs.umur;
+}
